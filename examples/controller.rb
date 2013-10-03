2c172148a4ff186e7c13bcb2bfc3a7a0a972662d
deadprogram <ron@hybridgroup.com>
2013-10-02 22:06:01 -0700

    Small naming improvement

--- examples/controller.rb
+++ examples/controller.rb
@@ -1,13 +1,13 @@
 require 'artoo'
 
 connection :joystick, :adaptor => :sdl_joystick
-device :joystick, :driver => :sdl_joystick, :connection => :joystick, :interval => 0.1
+device :controller, :driver => :sdl_joystick, :connection => :joystick, :interval => 0.1
 
 work do
-  on joystick, :joystick => proc { |*value|
+  on controller, :joystick => proc { |*value|
     puts "joystick x:#{value[1][:x]} y:#{value[1][:y]}"
   }
-  on joystick, :button => proc { |*value|
+  on controller, :button => proc { |*value|
     puts "button #{value[1]}"
   }
 end
