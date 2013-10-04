8f01065dd2bd75f2135ab7bd088701be9cf7c803
Adrian Zankich <adrian@hybridgroup.com>
2013-10-04 13:22:44 -0700

    Add ps3 controller

---
+++ examples/ps3_controller.rb
@@ -0,0 +1,16 @@
+require 'artoo'
+
+connection :joystick, :adaptor => :joystick
+device :controller, :driver => :ps3, :connection => :joystick, :interval => 0.1
+
+work do
+  on controller, :joystick => proc { |*value|
+    puts "joystick #{value[1][:s]} x:#{value[1][:x]} y:#{value[1][:y]}"
+  }
+  on controller, :button => proc { |*value|
+    puts "button #{value[1]}"
+  }
+  on controller, :button_square => proc { |*value|
+    puts "l7 weenie!"
+  }
+end
---
+++ lib/artoo/drivers/ps3.rb
@@ -0,0 +1,43 @@
+require 'artoo/drivers/joystick'
+
+module Artoo
+  module Drivers
+    # The Ps3 controller driver behaviors
+    class Ps3 < Artoo::Drivers::Joystick
+      BUTTON_MAP = {
+        0 => 'square',
+        1 => 'x',
+        2 => 'circle',
+        3 => 'triangle',
+        4 => 'l1',
+        5 => 'r1',
+        6 => 'l2',
+        7 => 'r2',
+        8 => 'select',
+        9 => 'start',
+        10 => 'left_joystick',
+        11 => 'right_joystick',
+        12 => 'home'
+      }
+
+      def handle_joystick
+        x0 = connection.axis(0)
+        y0 = connection.axis(1)
+        publish_joystick(0, x0, y0)
+
+        x1 = connection.axis(2)
+        y1 = connection.axis(3)
+        publish_joystick(1, x1, y1)        
+      end
+
+      def publish_button(b)
+        publish(event_topic_name("button_#{button_letter(b)}"))
+        super
+      end
+
+      def button_letter(b)
+        BUTTON_MAP[b]
+      end
+    end
+  end
+end
