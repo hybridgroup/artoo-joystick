d95302c981ce81147e0f0b8b16dd033daca1afb6
deadprogram <ron@hybridgroup.com>
2013-10-04 13:11:37 -0700

    Improve xbox360 controller example

--- examples/xbox_360_controller.rb
+++ examples/xbox_360_controller.rb
@@ -7,10 +7,16 @@ work do
   on controller, :joystick => proc { |*value|
     puts "joystick #{value[1][:s]} x:#{value[1][:x]} y:#{value[1][:y]}"
   }
-  on controller, :button => proc { |*value|
-    puts "button #{value[1]}"
-  }
   on controller, :button_a => proc { |*value|
     puts "ayyyy!"
+  }
+  on controller, :button_b => proc { |*value|
+    puts "bee!"
+  }
+  on controller, :button_x => proc { |*value|
+    puts "exxx!"
+  }
+  on controller, :button_y => proc { |*value|
+    puts "why!"
   }
 end
