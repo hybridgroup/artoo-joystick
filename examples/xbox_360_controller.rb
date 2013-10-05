a369da512e425c51abce7fd66850fba65efecd2c
deadprogram <ron@hybridgroup.com>
2013-10-04 19:48:58 -0700

    Better example

--- examples/xbox_360_controller.rb
+++ examples/xbox_360_controller.rb
@@ -4,8 +4,11 @@ connection :joystick, :adaptor => :joystick
 device :controller, :driver => :xbox360, :connection => :joystick, :interval => 0.1
 
 work do
-  on controller, :joystick => proc { |*value|
-    puts "joystick #{value[1][:s]} x:#{value[1][:x]} y:#{value[1][:y]}"
+  on controller, :joystick_0 => proc { |*value|
+    puts "joystick 0 - x:#{value[1][:x]} y:#{value[1][:y]}"
+  }
+  on controller, :joystick_1 => proc { |*value|
+    puts "joystick 1 - x:#{value[1][:x]} y:#{value[1][:y]}"
   }
   on controller, :button_a => proc { |*value|
     puts "ayyyy!"
